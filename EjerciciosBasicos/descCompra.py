575f271712a6c0e5bd271b7ee78b191a4ff721ad
Juanvi1905 <Takzuin@gmail.com>
2024-08-30 20:27:39 -0500

    Update descCompra.py

--- EjerciciosBasicos/descCompra.py
+++ EjerciciosBasicos/descCompra.py
@@ -1,8 +1,12 @@
 precio_original = float(input("Ingrese el precio del producto: "))
 descuento = float(input("Ingrese el porcentaje de descuento: "))
 
+
+iva = 19
 descuento_cantidad = precio_original * (descuento / 100)
 precio_final = precio_original - descuento_cantidad
+precio_IVA = precio_final * (iva / 100)
 
 print("El descuento es de:", descuento_cantidad)
 print("El precio final es:", precio_final)
+print("Precio adicional por IVA:", precio_IVA)
