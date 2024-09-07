f4a836adbf54728a62f9be2045d9ba1243633d57
Juanvi1905 <Takzuin@gmail.com>
2024-09-06 21:00:14 -0500

    Update

---
+++ DiseñoAlgoritmosI/EjerciciosCondicionales/1.py
@@ -0,0 +1,9 @@
+#Verifica si 'x' está entre 'y' y 'z', y si 'w' es par:
+x, y, z, w = 15, 10, 20, 8
+
+if x >= y and x <= z:
+    (print("x esta entre y y z"))
+    if w % 2 == 0:
+        print("Par")
+    else:
+        print("Impar")
---
+++ DiseñoAlgoritmosI/EjerciciosCondicionales/3.py
@@ -0,0 +1,8 @@
+#Verifica si 'x' es negativo, 'y' es positivo, y 'z' es cero:
+x, y, z = -3, 5, 0
+if x <= 0:
+    print('N es negativo')
+if y >= 0:
+    print('Y es positivo')
+if z == 0:
+    print('Z es igual a 0')
