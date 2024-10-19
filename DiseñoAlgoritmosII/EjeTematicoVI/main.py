acfd43e8837cf0f695575eb428d2945af0c9efbf
TAKZUIN <Takzuin@gmail.com>
2024-10-19 14:34:24 -0600

    Update

---
+++ DiseñoAlgoritmosII/EjeTematicoVI/main.py
@@ -0,0 +1,14 @@
+
+
+def menu():
+    print("\n--- Cajero Automático ---")
+    print("1. Consultar saldo")
+    print("2. Depositar")
+    print("3. Retirar")
+    print("4. Salir")
+    return input("Seleccione una opción: ")
+
+
+if __name__ == '__main__':
+   menu()
+
